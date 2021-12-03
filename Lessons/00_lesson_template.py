b2b769bec0312443458d710b2bb3be6480534a0f
Lukas <Lukas.Erekson@gmail.com>
2021-12-02 19:47:03 -0700

    Create Lesson Template

---
+++ Lessons/00_lesson_template.py
@@ -0,0 +1,100 @@
+import sqlite3
+import pandas as pd
+
+def main():
+    """Runs and prints the results of the queries required in the second lesson
+    of the SQL Zoo, SELECT from world.
+    """
+    try:
+        conn = sqlite3.connect('databases/world.db')
+        cur = conn.cursor()
+
+        # Problem 1
+        print(f"Problem 1:\n{prob1(cur)}\n")
+
+        # Problem 2
+        print(f"Problem 2:\n{prob2(cur)}\n")
+
+        # Problem 3
+        print(f"Problem 3:\n{prob3(cur)}\n")
+
+        # Problem 4
+        print(f"Problem 4:\n{prob4(cur)}\n")
+
+        # Problem 5
+        print(f"Problem 5:\n{prob5(cur)}\n")
+
+        # Problem 6
+        print(f"Problem 6:\n{prob6(cur)}\n")
+
+        # Problem 7
+        print(f"Problem 7:\n{prob7(cur)}\n")
+
+        # Problem 8
+        print(f"Problem 8:\n{prob8(cur)}\n")
+
+        # Problem 9
+        print(f"Problem 9:\n{prob9(cur)}\n")
+
+        # Problem 10
+        print(f"Problem 10:\n{prob10(cur)}\n")
+
+        # Problem 11
+        print(f"Problem 11:\n{prob11(cur)}\n")
+
+        # Problem 12
+        print(f"Problem 12:\n{prob12(cur)}\n")
+
+        # Problem 13
+        print(f"Problem 13:\n{prob13(cur)}\n")
+
+        # Problem 14
+        print(f"Problem 14:\n{prob14(cur)}\n")
+
+    finally:
+        conn.close()
+
+def prob1(cur: sqlite3.Cursor) -> pd.DataFrame:
+    pass
+
+def prob2(cur: sqlite3.Cursor) -> pd.DataFrame:
+    pass
+
+def prob3(cur: sqlite3.Cursor) -> pd.DataFrame:
+    pass
+
+def prob4(cur: sqlite3.Cursor) -> pd.DataFrame:
+    pass
+
+def prob5(cur: sqlite3.Cursor) -> pd.DataFrame:
+    pass
+
+def prob6(cur: sqlite3.Cursor) -> pd.DataFrame:
+    pass
+
+def prob7(cur: sqlite3.Cursor) -> pd.DataFrame:
+    pass
+
+def prob8(cur: sqlite3.Cursor) -> pd.DataFrame:
+    pass
+
+def prob9(cur: sqlite3.Cursor) -> pd.DataFrame:
+    pass
+
+def prob10(cur: sqlite3.Cursor) -> pd.DataFrame:
+    pass
+
+def prob11(cur: sqlite3.Cursor) -> pd.DataFrame:
+    pass
+
+def prob12(cur: sqlite3.Cursor) -> pd.DataFrame:
+    pass
+
+def prob13(cur: sqlite3.Cursor) -> pd.DataFrame:
+    pass
+
+def prob14(cur: sqlite3.Cursor) -> pd.DataFrame:
+    pass
+
+if __name__ == "__main__":
+    main()
