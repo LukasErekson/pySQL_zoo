43e7505683ad745365640bc03d932936de1ca7e3
Lukas <Lukas.Erekson@gmail.com>
2021-12-13 22:07:00 -0700

    Complete Lesson 5

--- Lessons/05_join.py
+++ Lessons/05_join.py
@@ -48,15 +48,12 @@ def main():
         # Problem 13
         print(f"Problem 13:\n{prob13(cur)}\n")
 
-        # Problem 14
-        print(f"Problem 14:\n{prob14(cur)}\n")
-
     finally:
         conn.close()
 
 def prob1(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """ show the matchid and player name for all goals scored by Germany. 
-    
+    """Show the matchid and player name for all goals scored by Germany.
+
     Parameters
     ----------
         cur (sqlite3.Cursor) : The cursor for the database we're accessing.
@@ -67,11 +64,11 @@ def prob1(cur: sqlite3.Cursor) -> pd.DataFrame:
     """
     cur.execute("SELECT matchid, player FROM goal WHERE teamid = 'GER';")
 
-    return pd.DataFrame(cur.fetchall(), columns=['matchid', 'player'])
+    return pd.DataFrame(cur.fetchall())
 
 def prob2(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """
-    
+    """Show id, stadium, team1, team2 for just game 1012.
+
     Parameters
     ----------
         cur (sqlite3.Cursor) : The cursor for the database we're accessing.
@@ -80,13 +77,14 @@ def prob2(cur: sqlite3.Cursor) -> pd.DataFrame:
     -------
         (pd.DataFrame) : Table with the solution.
     """
-    cur.execute("")
+    cur.execute("SELECT id, stadium, team1, team2 FROM game WHERE id = 1012;")
 
-    return pd.DataFrame(cur.fetchall(), columns=['yr', 'subject', 'winner'])
+    return pd.DataFrame(cur.fetchall())
 
 def prob3(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """
-    
+    """Modify it to show the player, teamid, stadium and mdate for every German
+    goal.
+
     Parameters
     ----------
         cur (sqlite3.Cursor) : The cursor for the database we're accessing.
@@ -95,13 +93,14 @@ def prob3(cur: sqlite3.Cursor) -> pd.DataFrame:
     -------
         (pd.DataFrame) : Table with the solution.
     """
-    cur.execute("")
+    cur.execute("SELECT goal.player, goal.teamid, game.stadium, game.mdate FROM goal JOIN game ON (game.id = goal.matchid) WHERE goal.teamid = 'GER';")
 
-    return pd.DataFrame(cur.fetchall(), columns=['yr', 'subject', 'winner'])
+    return pd.DataFrame(cur.fetchall())
 
 def prob4(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """
-    
+    """Show the team1, team2 and player for every goal scored by a player
+    called Mario using player LIKE 'Mario%'.
+
     Parameters
     ----------
         cur (sqlite3.Cursor) : The cursor for the database we're accessing.
@@ -110,13 +109,14 @@ def prob4(cur: sqlite3.Cursor) -> pd.DataFrame:
     -------
         (pd.DataFrame) : Table with the solution.
     """
-    cur.execute("")
+    cur.execute("SELECT game.team1, game.team2, goal.player FROM goal JOIN game ON (game.id = goal.matchid) WHERE goal.player LIKE 'Mario%';")
 
-    return pd.DataFrame(cur.fetchall(), columns=['yr', 'subject', 'winner'])
+    return pd.DataFrame(cur.fetchall())
 
 def prob5(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """
-    
+    """Show player, teamid, coach, gtime for all goals scored in the first 10
+    minutes gtime<=10.
+
     Parameters
     ----------
         cur (sqlite3.Cursor) : The cursor for the database we're accessing.
@@ -125,13 +125,16 @@ def prob5(cur: sqlite3.Cursor) -> pd.DataFrame:
     -------
         (pd.DataFrame) : Table with the solution.
     """
-    cur.execute("")
+    cur.execute("""SELECT player, teamid, coach, gtime
+                FROM goal JOIN eteam ON goal.teamid = eteam.id
+                WHERE goal.gtime <= 10;""")
 
-    return pd.DataFrame(cur.fetchall(), columns=['yr', 'subject', 'winner'])
+    return pd.DataFrame(cur.fetchall())
 
 def prob6(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """
-    
+    """List the dates of the matches and the name of the team in which
+    'Fernando Santos' was the team1 coach.
+
     Parameters
     ----------
         cur (sqlite3.Cursor) : The cursor for the database we're accessing.
@@ -140,13 +143,16 @@ def prob6(cur: sqlite3.Cursor) -> pd.DataFrame:
     -------
         (pd.DataFrame) : Table with the solution.
     """
-    cur.execute("")
+    cur.execute("""SELECT game.mdate, eteam.teamname
+                FROM game JOIN eteam ON (eteam.id = game.team1)
+                WHERE eteam.coach = 'Fernando Santos';""")
 
-    return pd.DataFrame(cur.fetchall(), columns=['yr', 'subject', 'winner'])
+    return pd.DataFrame(cur.fetchall())
 
 def prob7(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """
-    
+    """List the player for every goal scored in a game where the stadium was
+    'National Stadium, Warsaw'.
+
     Parameters
     ----------
         cur (sqlite3.Cursor) : The cursor for the database we're accessing.
@@ -155,13 +161,15 @@ def prob7(cur: sqlite3.Cursor) -> pd.DataFrame:
     -------
         (pd.DataFrame) : Table with the solution.
     """
-    cur.execute("")
+    cur.execute("""SELECT goal.player
+                FROM goal JOIN game ON (goal.matchid = game.id)
+                WHERE game.stadium = 'National Stadium, Warsaw';""")
 
-    return pd.DataFrame(cur.fetchall(), columns=['yr', 'subject', 'winner'])
+    return pd.DataFrame(cur.fetchall())
 
 def prob8(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """
-    
+    """Show the name of all players who scored a goal against Germany.
+
     Parameters
     ----------
         cur (sqlite3.Cursor) : The cursor for the database we're accessing.
@@ -170,28 +178,34 @@ def prob8(cur: sqlite3.Cursor) -> pd.DataFrame:
     -------
         (pd.DataFrame) : Table with the solution.
     """
-    cur.execute("")
+    cur.execute("""SELECT DISTINCT player
+                FROM game
+                JOIN goal ON game.id = goal.matchid
+                WHERE (team1 = 'GER' OR team2 = 'GER')
+                AND goal.teamid <> 'GER';""")
 
-    return pd.DataFrame(cur.fetchall(), columns=['yr', 'subject', 'winner'])
+    return pd.DataFrame(cur.fetchall())
 
 def prob9(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """
-    
+    """Show teamname and the total number of goals scored.
+
     Parameters
     ----------
         cur (sqlite3.Cursor) : The cursor for the database we're accessing.
-
     Returns
     -------
         (pd.DataFrame) : Table with the solution.
     """
-    cur.execute("")
+    cur.execute("""SELECT eteam.teamname, COUNT(goal.player) AS goal_count
+                FROM eteam
+                JOIN goal ON eteam.id = goal.teamid
+                GROUP BY eteam.teamname;""")
 
-    return pd.DataFrame(cur.fetchall(), columns=['yr', 'subject', 'winner'])
+    return pd.DataFrame(cur.fetchall())
 
 def prob10(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """
-    
+    """Show the stadium and the number of goals scored in each stadium.
+
     Parameters
     ----------
         cur (sqlite3.Cursor) : The cursor for the database we're accessing.
@@ -200,13 +214,17 @@ def prob10(cur: sqlite3.Cursor) -> pd.DataFrame:
     -------
         (pd.DataFrame) : Table with the solution.
     """
-    cur.execute("")
+    cur.execute("""SELECT game.stadium, COUNT(goal.player) AS total_goals
+                    FROM game
+                    JOIN goal ON game.id = goal.matchid
+                    GROUP BY game.stadium;""")
 
-    return pd.DataFrame(cur.fetchall(), columns=['yr', 'subject', 'winner'])
+    return pd.DataFrame(cur.fetchall())
 
 def prob11(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """
-    
+    """For every match involving 'POL', show the matchid, date and the number
+    of goals scored.
+
     Parameters
     ----------
         cur (sqlite3.Cursor) : The cursor for the database we're accessing.
@@ -215,13 +233,19 @@ def prob11(cur: sqlite3.Cursor) -> pd.DataFrame:
     -------
         (pd.DataFrame) : Table with the solution.
     """
-    cur.execute("")
+    cur.execute("""SELECT DISTINCT goal.matchid, game.mdate,
+                COUNT(goal.player) AS goals
+                FROM game
+                JOIN goal ON game.id = goal.matchid
+                WHERE game.team1 = 'POL' OR game.team2 = 'POL'
+                GROUP BY goal.matchid, game.mdate;""")
 
-    return pd.DataFrame(cur.fetchall(), columns=['yr', 'subject', 'winner'])
+    return pd.DataFrame(cur.fetchall())
 
 def prob12(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """
-    
+    """For every match where 'GER' scored, show matchid, match date and the
+    number of goals scored by 'GER'.
+
     Parameters
     ----------
         cur (sqlite3.Cursor) : The cursor for the database we're accessing.
@@ -230,13 +254,18 @@ def prob12(cur: sqlite3.Cursor) -> pd.DataFrame:
     -------
         (pd.DataFrame) : Table with the solution.
     """
-    cur.execute("")
+    cur.execute("""SELECT goal.matchid, game.mdate,
+                COUNT(goal.player) AS goals_scored
+                FROM goal JOIN game ON goal.matchid = game.id
+                WHERE goal.teamid = 'GER'
+                GROUP BY goal.matchid, game.mdate;""")
 
-    return pd.DataFrame(cur.fetchall(), columns=['yr', 'subject', 'winner'])
+    return pd.DataFrame(cur.fetchall())
 
 def prob13(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """
-    
+    """List every match with the goals scored by each team as shown. This will
+    use "CASE WHEN" which has not been explained in any previous exercises.
+
     Parameters
     ----------
         cur (sqlite3.Cursor) : The cursor for the database we're accessing.
@@ -245,24 +274,17 @@ def prob13(cur: sqlite3.Cursor) -> pd.DataFrame:
     -------
         (pd.DataFrame) : Table with the solution.
     """
-    cur.execute("")
+    cur.execute("""SELECT game.mdate,
+                game.team1,
+                SUM(CASE WHEN teamid = team1 THEN 1 ELSE 0 END) AS score1,
+                game.team2,
+                SUM(CASE WHEN teamid = team2 THEN 1 ELSE 0 END) AS score2
+                FROM game
+                LEFT JOIN goal ON goal.matchid = game.id
+                GROUP BY mdate, team1, team2
+                ORDER BY game.mdate, goal.matchid, game.team1, game.team2;""")
 
-    return pd.DataFrame(cur.fetchall(), columns=['yr', 'subject', 'winner'])
-
-def prob14(cur: sqlite3.Cursor) -> pd.DataFrame:
-    """
-    
-    Parameters
-    ----------
-        cur (sqlite3.Cursor) : The cursor for the database we're accessing.
-
-    Returns
-    -------
-        (pd.DataFrame) : Table with the solution.
-    """
-    cur.execute("")
-
-    return pd.DataFrame(cur.fetchall(), columns=['yr', 'subject', 'winner'])
+    return pd.DataFrame(cur.fetchall())
 
 if __name__ == "__main__":
     main()
